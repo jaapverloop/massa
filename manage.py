a3923263a100dd39772533aa37ea7ff956e6c874
Jaap Verloop <j.verloop@gmail.com>
2013-12-03 17:42:32 +0100

    Make app accessible outside the development machine.

--- manage.py
+++ manage.py
@@ -7,5 +7,12 @@ from yoyo import create_app
 manager = Manager(create_app)
 manager.add_option('-c', '--configfile', dest='configfile', required=False)
 
+manager.add_command('runserver', Server(
+    use_debugger = True,
+    use_reloader = True,
+    host = '0.0.0.0',
+    port = 8080,
+))
+
 if __name__ == '__main__':
     manager.run()
