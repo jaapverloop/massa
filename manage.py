9272e0fb2f3c3190f53ea440a9d15610667d6072
Jaap Verloop <j.verloop@gmail.com>
2013-12-16 19:50:11 +0100

    Add commands to make and drop db tables from a terminal.

--- manage.py
+++ manage.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 
-from flask.ext.script import Manager, Server
+from flask.ext.script import Manager, Server, prompt_bool
 from massa import create_app
+from massa.domain import make_tables, drop_tables
 
 
 manager = Manager(create_app)
@@ -13,6 +14,17 @@ manager.add_command('runserver', Server(
     host = '0.0.0.0',
     port = 8080,
 ))
+
+@manager.command
+def dbmake():
+  """Make all the db tables."""
+  make_tables()
+
+@manager.command
+def dbdrop():
+  """Drop all the db tables."""
+  if prompt_bool('Are you sure you want to drop all the db tables?'):
+    drop_tables()
 
 if __name__ == '__main__':
     manager.run()
