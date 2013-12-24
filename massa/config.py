b236f805ce1cedc7bd099cc6776119edd08efebc
Jaap Verloop <j.verloop@gmail.com>
2013-12-24 17:52:23 +0100

    Define config variable `TESTING`.

--- massa/config.py
+++ massa/config.py
@@ -5,6 +5,7 @@ import logging
 
 class Production(object):
     DEBUG = False
+    TESTING = False
     SECRET_KEY = '##CHANGEME##'
     SQLALCHEMY_DATABASE_URI = 'postgresql://massa:secret@localhost/massa'
     SQLALCHEMY_ECHO = False
