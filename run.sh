5bae58ff37bdb3e4297f48aeb062ba6d2e3a3c94
Jaap Verloop <j.verloop@gmail.com>
2014-03-05 20:40:03 +0100

    Add a '.env' file as a requirement for the run script. An example file is located in the root directory.

--- run.sh
+++ run.sh
@@ -1,6 +1,15 @@
 #!/usr/bin/env bash
 
 MASSA_ROOT="$(cd "$(dirname "${0}")"; echo $(pwd))"
+ENV_FILE="${MASSA_ROOT}/.env"
+
+if [ ! -f "${ENV_FILE}" ]; then
+    echo "The root directory does not contain the required `.env` file."
+    echo "Copy the example file and make the necessary changes."
+    exit 1
+fi
+
+source "${ENV_FILE}"
 
 exec "${MASSA_ROOT}/.virtualenv/bin/gunicorn" \
     --name massa \
